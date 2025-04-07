afacb5924baf7323c5ae603c66e897c17cc5be04
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-07 09:42:29 +0600

    initial commit with Maven setup and docker PostgreSQL configuration

---
+++ src/main/java/com/kolu/ecombackend/EcomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kolu.ecombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kolu/ecombackend/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolu.ecombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
