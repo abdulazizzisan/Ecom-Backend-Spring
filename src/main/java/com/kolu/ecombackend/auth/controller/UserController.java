988b71666378ab6418d722462eb7959dfb1f5a4d
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-08 11:21:28 +0600

    add password length check and API documentation for /register endpoint

--- src/main/java/com/kolu/ecombackend/auth/controller/UserController.java
+++ src/main/java/com/kolu/ecombackend/auth/controller/UserController.java
@@ -3,16 +3,25 @@ package com.kolu.ecombackend.auth.controller;
 import com.kolu.ecombackend.auth.model.Roles;
 import com.kolu.ecombackend.auth.model.dto.AuthResponse;
 import com.kolu.ecombackend.auth.model.dto.RegisterRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User Authentication")
 @RestController
 @RequestMapping("/api/v1/auth/user")
 public class UserController {
+    @Operation(
+            summary = "Register a new user",
+            description = """
+                    -> Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.\n
+                    -> Phone number must be a valid Bangladeshi phone number starting with +8801 and followed by 9 digits.
+                    """
+    )
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> registerUser(
             @RequestBody
--- src/main/java/com/kolu/ecombackend/auth/model/dto/RegisterRequest.java
+++ src/main/java/com/kolu/ecombackend/auth/model/dto/RegisterRequest.java
@@ -3,6 +3,7 @@ package com.kolu.ecombackend.auth.model.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
@@ -15,6 +16,7 @@ public record RegisterRequest(
         @NotBlank(message = "Email is required")
         @Email(message = "Email is not valid")
         String email,
+        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
         @Pattern(
                 regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$",
                 message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, and be at least 8 characters long"
