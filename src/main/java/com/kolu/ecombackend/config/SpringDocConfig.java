0b6b77d06a8b9564b69d0ffadf5c8ecfc89c1d5c
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-05-04 09:51:47 +0600

    add SpringDoc configuration for API documentation and JWT authentication

---
+++ src/main/java/com/kolu/ecombackend/config/SpringDocConfig.java
@@ -0,0 +1,42 @@
+package com.kolu.ecombackend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SpringDocConfig {
+    @Bean
+    public OpenAPI springDocOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("E-commerce Backend API")
+                        .version("0.1.0")
+                        .description("API documentation for the E-commerce backend application.")
+                )
+                .servers(
+                        List.of(new Server()
+                                .url("http://localhost:8080")
+                                .description("Local server")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes(
+                        "bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
+                                .description("JWT authentication")
+                ));
+    }
+}
