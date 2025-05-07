c33e4c92c0ec66c6d8137b8a6fb22556919d1396
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-05-08 00:40:01 +0600

    role based access

--- src/main/java/com/kolu/ecombackend/hello/HelloController.java
+++ src/main/java/com/kolu/ecombackend/hello/HelloController.java
@@ -1,11 +1,13 @@
 package com.kolu.ecombackend.hello;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/hello")
+@PreAuthorize("hasRole('USER')")
 public class HelloController {
     @GetMapping
     public String hello() {
