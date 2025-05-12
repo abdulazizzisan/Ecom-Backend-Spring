b782cfc755e74b21637552d5ec32c76ccaa970d0
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-05-12 11:48:43 +0600

    adding support for ngRok server in SpringDocConfig.java

--- src/main/java/com/kolu/ecombackend/config/SpringDocConfig.java
+++ src/main/java/com/kolu/ecombackend/config/SpringDocConfig.java
@@ -1,10 +1,10 @@
 package com.kolu.ecombackend.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -14,11 +14,15 @@ import java.util.List;
 
 @Configuration
 public class SpringDocConfig {
+
+    @Value("${NG_ROK_URL}")
+    private String ngrokUrl;
+
     @Bean
     public OpenAPI springDocOpenAPI() {
         var servers = List.of(
                 new Server().url("http://localhost:8080").description("Local server"),
-                new Server().url("").description("ngrok server")
+                new Server().url(ngrokUrl).description("ngrok server")
         );
         return new OpenAPI()
                 .info(new Info()
